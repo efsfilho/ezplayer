a0cf438e69b64672bfec24a020726e79d98a86a1
efsfilho <efdsfilho@gmail.com>
2024-03-24 13:06:59 -0300

    config

--- go.mod
+++ go.mod
@@ -16,4 +16,5 @@ require (
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -11,9 +11,82 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gopkg.in/yaml.v3"
 )
 
-var storage = "/"
+const CONFIG_FILE = "config.yaml"
+
+type Config struct {
+	Server struct {
+		Port uint16 `yaml:"port"`
+	}
+	Eznvr struct {
+		Storage string `yaml:"storage"`
+	}
+}
+
+var (
+	SERVER_PORT   uint16
+	EZNVR_STORAGE string
+)
+
+func loadConfig() {
+	var config Config
+
+	// Cheks config file
+	createFile := false
+	if _, err := os.Stat(CONFIG_FILE); err != nil {
+		if createFile = os.IsNotExist(err); !createFile {
+			log.Fatal(err)
+		}
+	}
+
+	// // Creates a default config
+	// if createFile {
+	// 	conf = Config{
+	// 		struct {
+	// 			Port uint16 "yaml:\"port\""
+	// 		}{4000},
+	// 		struct {
+	// 			Storage string "yaml:\"storage\""
+	// 		}{"/path/to/eznvr/storage"},
+	// 	}
+	// 	var out []byte
+	// 	var newFile *os.File
+	// 	var err error
+
+	// 	if out, err = yaml.Marshal(conf); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	if newFile, err = os.Create(CONFIG_FILE); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	defer newFile.Close()
+	// 	if _, err = newFile.Write(out); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+
+	// Reads config
+	var file []byte
+	var err error
+	if file, err = os.ReadFile(CONFIG_FILE); err != nil {
+		log.Fatal(err)
+	}
+	if err = yaml.Unmarshal(file, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	SERVER_PORT = config.Server.Port
+	EZNVR_STORAGE = config.Eznvr.Storage
+
+	if _, err := os.Stat(EZNVR_STORAGE); err != nil {
+		if os.IsNotExist(err) {
+			log.Fatal(err)
+		}
+	}
+	log.Println("EZ-NVR storage:", EZNVR_STORAGE)
+}
 
 type Video struct {
 	Name             string    `json:"name"`
@@ -22,6 +95,7 @@ type Video struct {
 }
 
 func main() {
+	loadConfig()
 
 	e := echo.New()
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
@@ -35,24 +109,26 @@ func main() {
 
 	// Routes
 	e.Static("/", "web")
-	e.Static("/recordings", storage)
+	e.Static("/recordings", EZNVR_STORAGE) //TODO
 	e.GET("/list-cams", listCams)
 	e.GET("/list-videos/:cam", listVideos)
 
 	// Start server
-	e.Logger.Fatal(e.Start(":4000"))
+	e.HideBanner = true
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", SERVER_PORT)))
 }
 
 func listCams(c echo.Context) error {
-
+	log.Println()
 	var cams []string
-	files, err := os.ReadDir(storage)
+	files, err := os.ReadDir(EZNVR_STORAGE)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, file := range files {
+		// TODO remove filter
 		if strings.HasPrefix(file.Name(), "portao") {
 			cams = append(cams, file.Name())
 			fmt.Println(file.Name(), file.IsDir())
@@ -65,13 +141,11 @@ func listCams(c echo.Context) error {
 func listFiles(vid Video) []Video {
 
 	var videos []Video
-	files, err := os.ReadDir(vid.Location)
-
+	dirEntry, err := os.ReadDir(vid.Location)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for _, file := range files {
+	for _, file := range dirEntry {
 		filePath := path.Join(vid.Location, file.Name())
 		if file.IsDir() {
 			video := Video{
@@ -89,7 +163,7 @@ func listFiles(vid Video) []Video {
 					lastModification = info.ModTime()
 				}
 				location := strings.Replace(filePath, "..", "", 1)
-				location = strings.Replace(location, storage, "", 1)
+				location = strings.Replace(location, EZNVR_STORAGE, "", 1)
 				video := Video{
 					path.Base(filePath),
 					location,
@@ -99,14 +173,13 @@ func listFiles(vid Video) []Video {
 			}
 		}
 	}
-
 	return videos
 }
 
 func listVideos(c echo.Context) error {
 
 	cam := c.Param("cam")
-	videosPath := path.Join(storage, cam)
+	videosPath := path.Join(EZNVR_STORAGE, cam)
 	_, err := os.Stat(videosPath)
 
 	if err != nil {
